c0363941ffe00e4ad66e68f6fb765e7eba4e0a25
davidhayden <davidhayden@users.noreply.github.com>
2018-01-27 22:50:46 -0500

    Adding errors blueprint.

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
+from app.errors import bp as errors_bp
+app.register_blueprint(errors_bp)
+
 from app.main import bp as main_bp
 app.register_blueprint(main_bp)
