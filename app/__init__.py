7228cca6bda236ec4b1602bfc0f87cb3587e06ca
davidhayden <davidhayden@users.noreply.github.com>
2018-01-28 17:16:02 -0500

    Adding config and env variables support.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
+from config import Config
 from flask import Flask
 
 
 app = Flask(__name__)
+
+app.config.from_object(Config)
 
 from app.errors import bp as errors_bp
 app.register_blueprint(errors_bp)
